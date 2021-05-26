76143acda402470d1aa2501e72171115f225ef81
Matveev Aleksey <matveevalex1512@gmail.com>
2021-05-27 01:11:33 +0300

    Refactor Paginator

--- classes/paginator.class.php
+++ classes/paginator.class.php
@@ -1,53 +1,72 @@
 <?php
-class Paginator{
-
-    public static function pagination($conn, $page, $limit)
+final class  Paginator
+{
+    private static $columnName = Array();
+    private static $body = Array();
+    private static $countPages;
+    private function __construct()
     {
-        $error      = "";
-        $status     = 1;
+    }
 
+    /**
+    * @return mixed
+    */
+    public static function getCountPages()
+    {
+        return self::$countPages;
+    }
+
+    public static function pagination($conn, $database, $table, $page, $limit)
+    {
+        $error  = "";
+        $status = 1;
+
+#region check page and limit
         if($page < 1) {
             $page = 1;
-            $error      .=  "Такой страницы не существует! ";
-            $status     = 0;
+            $error .= "Страницы с нулевым индексом не существует! PAGE установлен в значение 1. ";
+            $status = 0;
         }
-
-        switch ($limit){
-            case 0:{
-                $status = 0;
-                $error  .= "Не указан LIMIT. LIMIT установлен в значение 100. ";
-                $limit = 100;
-                break;
-            }
-            case 101:{
-                $status = 0;
-                $error .= "Превышен допустимый LIMIT. LIMIT установлен в значение 100. ";
-                break;
-            }
+        if($limit == 0 ) {
+            $status = 0;
+            $error .= "Не указан LIMIT. LIMIT установлен в значение 100. ";
+            $limit = 100;
         }
+        if($limit > 100) {
+            $status = 0;
+            $error .= "Превышен допустимый LIMIT. LIMIT установлен в значение 100. ";
+            $limit = 100;
+        }
+#endregion
 
-        $query = "SELECT * FROM test LIMIT " . (($page - 1) * $limit) . " , $limit";
+        $query = "SELECT * FROM $database.$table LIMIT " . (($page - 1) * $limit) . " , $limit";
         $result = $conn->query($query);
 
         while ($row = $result->fetch_row())
-            $body[] = $row;
+            self::$body[] = $row;
 
+        self::$countPages = count(self::$body);
 
-        if (!isset($body)) {
+        if (!self::$body) {
             $status = 0;
             $error = "Страницы с таким номером не существует";
-            $body[] = '';
+            self::$body = '';
         }
 
-        $query = "SELECT `COLUMN_NAME`FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='test'";
+        $query = "SELECT `COLUMN_NAME`FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='". $table . "'";
         $result = $conn->query($query);
 
         while ($row = $result->fetch_assoc())
-            $name[] = $row['COLUMN_NAME'];
+            self::$columnName[] = $row['COLUMN_NAME'];
 
-        $data['head'] = $name;
-        $data['body'] = $body;
+        $data['head'] = self::$columnName;
+        $data['body'] = self::$body;
 
         return json_encode(["status" => $status, "error" => $error, "data" => $data], JSON_UNESCAPED_UNICODE);
     }
+
+
+
+
+
 }
--- get_table_data.php
+++ get_table_data.php
@@ -4,16 +4,18 @@
         include 'classes/' . $class . '.class.php';
     });
 
-    $conn = DB::connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+    $conn = DB::connect(DB_HOST, DB_USER, DB_PASSWORD);
 
 
     $page = $_GET['page'] ?? 1;
-    $limit = $_GET['page'] ?? 0;
+    $limit = $_GET['limit'] ?? 0;
 
-    $json_result = Paginator::pagination($conn, $page, $limit);
+    $json_result = Paginator::pagination($conn, DB_DATABASE, "test", $page, $limit);
+    
 
 
     header('Content-Type: application/json;charset=utf-8');
     header("Content-Disposition: inline; filename=table_json.json");
 
     echo $json_result;
+
